b97a0e2ffd2af6ff67b237c91f82ebe138f6ac87
Bryan Nystrom <bnystrom@anl.gov>
2025-03-12 17:36:42 -0500

    Include color defs directly in install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-source ./utils.sh
+# source ./utils.sh
+# Color text escape codes
+# Normally we load this brom tuils.sh, but in this case we haven't gotten utils.sh het.
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m' # Bold yellow
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+BRIGHTWHITE='\33[1;37m'
+NC='\033[0m' # No Color
 
 echo -e "${GREEN}********************************************${NC}"
 echo -e "${GREEN}* ${CYAN}Starting CIP.io-Link Installation...   ${GREEN} *${NC}"
